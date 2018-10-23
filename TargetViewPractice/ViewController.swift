e61c8dbc921680b08f5afff29c87b067a6cb1d17
thunderlatent <>
2018-10-23 22:51:54 +0800

    Initial Commit

---
+++ TargetViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TargetViewPractice
+//
+//  Created by 陳裕銘 on 2018/10/23.
+//  Copyright © 2018 yuming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
